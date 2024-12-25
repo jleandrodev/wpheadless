9a5b26a9ab40fd9b78c48415076355510519226a
jleandrodev <jleandro.dev@gmail.com>
2024-12-25 11:49:29 -0300

    http headers adjustments

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: ["placehold.jp", "hml-blog-coliseu.seodev.ambienteseo.com.br"],
-  },
   async headers() {
     return [
       {
-        // Define os headers para todas as rotas
         source: "/(.*)",
         headers: [
           {
             key: "Content-Security-Policy",
             value: `
               default-src 'self'; 
-              script-src 'self' 'unsafe-inline' 'unsafe-eval'; 
+              script-src 'self' 'nonce-{generated_nonce}'; 
               style-src 'self' 'unsafe-inline'; 
               img-src 'self' data: https:; 
               connect-src 'self'; 
@@ -24,37 +20,15 @@ const nextConfig = {
               form-action 'self'; 
             `
               .replace(/\s{2,}/g, " ")
-              .trim(), // Remove espaços extras
+              .trim(),
           },
           {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
           {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
             key: "Strict-Transport-Security",
             value: "max-age=63072000; includeSubDomains; preload",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "no-referrer",
-          },
-          {
-            key: "Permissions-Policy",
-            value:
-              "geolocation=(), microphone=(), camera=(), fullscreen=(self), payment=()",
-          },
-          {
-            key: "Expect-CT",
-            value: "max-age=86400, enforce",
-          },
-          {
-            key: "Cache-Control",
-            value:
-              "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
           },
         ],
       },
