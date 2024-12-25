160a73309ba0c78702163227654f336a97c74f9d
jleandrodev <jleandro.dev@gmail.com>
2024-12-25 01:44:39 -0300

    doc: http headers added on

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,40 @@ const nextConfig = {
   images: {
     domains: ["placehold.jp", "hml-blog-coliseu.seodev.ambienteseo.com.br"],
   },
+  async headers() {
+    return [
+      {
+        // Define os headers para todas as rotas
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self'; object-src 'none';",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(), microphone=(), camera=()",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
